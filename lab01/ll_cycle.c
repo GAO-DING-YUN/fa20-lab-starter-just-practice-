4644d2c1d9f7fa4f92057b1b82b119aef3f3e0ab
GAO-DING-YUN <gdy111111@outlook.com>
2026-02-11 15:40:55 +0800

    cover

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,16 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    if (head == NULL) {
+        return 0;
+    }
+    node* p = head, *q = head;
+    while (p != NULL && p->next != NULL) {
+        p = p->next->next;
+        q = q->next;
+        if (p == q) {
+            return 1;
+        }
+    }
     return 0;
 }
--- lab02/vector.c
+++ lab02/vector.c
@@ -32,7 +32,7 @@ vector_t *bad_vector_new() {
     }
 
     retval->data[0] = 0;
-    return retval;
+    return retval; //返回了在栈上的内存空间
 }
 
 /* Another suboptimal way of creating a vector */
@@ -47,7 +47,7 @@ vector_t also_bad_vector_new() {
         allocation_failed();
     }
     v.data[0] = 0;
-    return v;
+    return v; //返回结构体效率低且结构体在栈上
 }
 
 /* Create a new vector with a size (length) of 1
@@ -58,27 +58,27 @@ vector_t *vector_new() {
     vector_t *retval;
 
     /* First, we need to allocate memory on the heap for the struct */
-    retval = /* YOUR CODE HERE */
+    retval = (vector_t*)malloc(sizeof(vector_t));/* YOUR CODE HERE */
 
     /* Check our return value to make sure we got memory */
-    if (/* YOUR CODE HERE */) {
+    if (!retval/* YOUR CODE HERE */) {
         allocation_failed();
     }
 
     /* Now we need to initialize our data.
        Since retval->data should be able to dynamically grow,
        what do you need to do? */
-    retval->size = /* YOUR CODE HERE */;
-    retval->data = /* YOUR CODE HERE */;
+    retval->size = 1/* YOUR CODE HERE */;
+    retval->data = (int*)malloc(sizeof(int) * retval->size)/* YOUR CODE HERE */;
 
     /* Check the data attribute of our vector to make sure we got memory */
-    if (/* YOUR CODE HERE */) {
+    if (!retval->data/* YOUR CODE HERE */) {
         free(retval);				//Why is this line necessary?
         allocation_failed();
     }
 
     /* Complete the initialization by setting the single component to zero */
-    /* YOUR CODE HERE */ = 0;
+    /* YOUR CODE HERE */ retval->data[0] = 0;
 
     /* and return... */
     return retval;
@@ -96,8 +96,8 @@ int vector_get(vector_t *v, size_t loc) {
     /* If the requested location is higher than we have allocated, return 0.
      * Otherwise, return what is in the passed location.
      */
-    if (loc < /* YOUR CODE HERE */) {
-        return /* YOUR CODE HERE */;
+    if (loc < v->size && loc >= 0/* YOUR CODE HERE */) {
+        return v->data[loc]/* YOUR CODE HERE */;
     } else {
         return 0;
     }
@@ -107,6 +107,8 @@ int vector_get(vector_t *v, size_t loc) {
    Remember, you need to free up ALL the memory that was allocated. */
 void vector_delete(vector_t *v) {
     /* YOUR SOLUTION HERE */
+    free(v->data);
+    free(v);
 }
 
 /* Set a value in the vector. If the extra memory allocation fails, call
@@ -117,4 +119,23 @@ void vector_set(vector_t *v, size_t loc, int value) {
      */
 
     /* YOUR SOLUTION HERE */
+    if(v == NULL) {
+        fprintf(stderr, "vector_get: passed a NULL vector.\n");
+        abort();
+    }
+
+    if (loc >= v->size) {
+        int *newData = (int*)realloc(v->data, (loc + 1) * sizeof(int));
+        if (!newData) {
+            allocation_failed();
+        }
+        for (size_t i = v->size; i <= loc; i++) {
+            newData[i] = 0;
+        }
+        v->size = loc + 1;
+        v->data = newData;
+        v->data[loc] = 0;
+    } else {
+        v->data[loc] = value;
+    }
 }
